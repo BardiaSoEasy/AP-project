d86bf61abbfd2a77d0d635115c0fad6c474261b6
Bardia Soizi <bardia.soizi@gmail.com>
2024-02-02 14:20:34 +0330

    template and forms

--- Phase2/clinicsystem/forms.py
+++ Phase2/clinicsystem/forms.py
@@ -1,5 +1,6 @@
 from django import forms
-from .import models
+from . import models
+
 class LoginForm(forms.Form):
     UserName=forms.CharField(required=True, label="User Name :")
     Password=forms.CharField(required=True, label="Password :", widget=forms.PasswordInput())
@@ -11,20 +12,61 @@ class RegisterForm(forms.Form):
     EMail=forms.CharField(required=True, label='EMail :')
     UserType=forms.ChoiceField(choices=[('Patient', 'Patient'), ('Staff', 'Staff')], label='User Type :')
 
-class ClinicIdChoiceField(forms.ModelChoiceField):
-    def label_from_instance(self, obj):
-        return obj.ClinicId
+class AppointmentForm(forms.ModelForm):
+    AppointmentId = forms.IntegerField(disabled=True, label='Appointment Id :')
+    Date = forms.CharField(label='Date :')
+    Time = forms.ChoiceField(label='Time :', choices=[('16:00','16:00'), ('16:30','16:30'), ('17:00', '17:00'), ('17:30', '17:30'), ('18:00', '18:00'), ('18:30', '18:30'), ('19:00', '19:00')])
+    Status = forms.CharField(disabled=True, label='Status :')
+    class Meta:
+        model=models.Appointment
+        fields=['AppointmentId', 'ClinicId', 'ServiceId', 'Date', 'Time', 'Status']
 
-class ServiceIdChoiceField(forms.ModelChoiceField):
-    def label_from_instance(self, obj):
-        return obj.ServiceId
+class ReserveForm(forms.ModelForm):
+    Date = forms.CharField(label='Date :')
+    Time = forms.ChoiceField(label='Time :', choices=[('16:00','16:00'), ('16:30','16:30'), ('17:00', '17:00'), ('17:30', '17:30'), ('18:00', '18:00'), ('18:30', '18:30'), ('19:00', '19:00')])
+    class Meta:
+        model=models.Appointment
+        fields=['ClinicId', 'ServiceId', 'Date', 'Time']
 
-class AppointmentForm(forms.Form):
-    AppointmentId = forms.IntegerField(required=False)
-    ClinicId = ClinicIdChoiceField(queryset=models.Clinic.objects.all(), required=True, label="Clinic Id :")
-    ServiceId = ServiceIdChoiceField(queryset=models.Service.objects.all(), required=True, label="Service Id :")
-    Year = forms.ChoiceField(choices=[(year, year) for year in range(2024, 2100)], initial=2024, label="Year :")
-    Month = forms.ChoiceField(choices=[(month, month) for month in range(1, 13)], initial=1, label="Month :")
-    Day = forms.ChoiceField(choices=[(day, day) for day in range(1, 32)], initial=1, label="Day :")
-    Time = forms.ChoiceField(choices=[('16:00','16:00'), ('16:30','16:30'), ('17:00', '17:00'), ('17:30', '17:30'), ('18:00', '18:00'), ('18:30', '18:30'), ('19:00', '19:00')], initial='16:00', label="Time :")
-    Status = forms.ChoiceField(choices=[('Booked','Booked'), ('Cancelled','Cancelled'), ('Paid','Paid')], initial='Booked', label="Status :")
+class ClinicForm(forms.ModelForm):
+    Name=forms.CharField(required=True, label="Name :")
+    Address=forms.CharField(required=True, label='Address :')
+    Contact=forms.CharField(required=True, label='Contact :')
+    class Meta:
+        model=models.Clinic
+        fields=['ClinicId', 'Name', 'Address', 'Contact']
+
+class ServiceForm(forms.ModelForm):
+    class Meta:
+        model=models.Service
+        fields=['ClinicId', 'Name', 'Price']
+
+class CreatePaymentForm(forms.ModelForm):
+    class Meta:
+        model=models.Payment
+        fields=['AppointmentId']
+
+class ClinicForm(forms.ModelForm):
+    ClinicId = forms.IntegerField(disabled=True, label='Clinic Id :')
+    Name = forms.CharField(label='Name :')
+    Address = forms.CharField(label='Address :')
+    Contact = forms.CharField(label='Contact :')
+    Availability = forms.BooleanField(label='Availability :')
+    class Meta:
+        model=models.Appointment
+        fields=['ClinicId', 'Name', 'Address', 'Contact', 'Availability']
+
+class ServiceForm(forms.ModelForm):
+    ServiceId = forms.IntegerField(disabled=True, label='Service Id :')
+    Name = forms.CharField(label='Name :')
+    Price = forms.IntegerField(label='Address :')
+    class Meta:
+        model=models.Appointment
+        fields=['ServiceId', 'ClinicId', 'Name', 'Price']
+
+class PaymentForm(forms.ModelForm):
+    PaymentId = forms.IntegerField(disabled=True, label='Payment Id :')
+    Amount = forms.IntegerField(label='Amount :')
+    class Meta:
+        model=models.Payment
+        fields=['PaymentId', 'AppointmentId', 'Amount']
